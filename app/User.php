2135ec1737d74e3c78b3f4081db2ea8d8687541e
Javier <javierbuenache86@gmail.com>
2019-11-26 21:22:33 +0100

    create_model_User

---
+++ app/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    public function books()
+    {
+        return $this->belongsToMany('App\Book', 'user_lend_books');
+    }
+    
+    public function register(Request $request)
+    {
+        $user = new Self();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+    }
+}
